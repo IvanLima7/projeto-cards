919f8e319d18adf650780f696f01c19d7bedae74
Gabriel Cards <43912656+GabrielC4rds@users.noreply.github.com>
2021-05-19 14:59:14 -0300

    Primeira aula
    Conceitos Styled Components
    
    Conceitos de Json
    
    Variaveis e constantes
    
    Css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import dados from "./data.json"
+
+const Holder = styled.div`
+  width: 100%;
+  height: auto;
+  background: #000;
+`;
+
+const Card = styled.div`
+  width: 30vw;
+  height: 20vh;
+  border-radius: 5px;
+  background: purple;
+  h1{
+    margin: 0;
+    font-size: 30px;
+    color: #fff;
+  }
+  p{
+    font-size: 20px;
+    color: #fff;
+  }
+`;
+
 
 function App() {
+  const title = "Conceitos do Styled Components"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Holder>
+      <Card>
+        <h1>{dados.title}</h1>
+        <p>{dados.description}</p>
+      </Card>
+      <Card>
+        <h1>{title}</h1>
+      </Card>
+    </Holder>
   );
 }
 
