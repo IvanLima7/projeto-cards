47c8dc99c71f7b8e6cdf1830ed726b166824bd6e
Gabriel Cards <43912656+GabrielC4rds@users.noreply.github.com>
2021-06-14 15:31:10 -0300

    update unstoppable

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,15 +1,18 @@
 import "../banner/style.css"
 
 function Banner() {
-    return (
-      <div className="MainBanner">
-          <div className="DivBanner">
-              <p className="TextoDivBanner">Gabriel Gado</p>
-              <img className="ImgBanner" src="./images/valorant-capa.jpg"/>
-          </div>
-          
+  return (
+    <div className="MainBanner">
+      <div className="ImgBanner">
+        <div className="DivBanner">
+
+          <p className="TextoDivBanner">Gabriel Unstoppable</p>
+        </div>
       </div>
-    );
-  }
+
+
+    </div>
+  );
+}
 
 export default Banner;
