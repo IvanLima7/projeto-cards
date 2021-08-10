b691aacb358da1bd04c0388d3575a023ae0e66f5
IvanLima7 <84291786+IvanLima7@users.noreply.github.com>
2021-08-10 08:39:18 -0300

    atualização 10/08

--- projetocards/src/components/atualizacao/index.js
+++ projetocards/src/components/atualizacao/index.js
@@ -3,16 +3,16 @@ import "./style.css"
 
 function Atualizacao(props) {
     return (
-        <div className="Main">
+        <div className="MainAtualizacao">
             <div className="MainImg">
-                <img className="Image" src="./images/CAPA-ATUALIZACAO-COPIA.png" />
+                <img className="Image" src="./images/ValorantAgente14.jpg" />
             </div>
             <div className="MainTexto">
                 <h2 className="TextoTitulo">Nova atualização do Valorant</h2>
-                <p className="TextoParagrafo">Nessa nova atualização, o valorant vem trazendo novas armas, skins, mapas e tambem novos 
-                    personagens com habilidades unicas e novas artes. confira mais sobre essa nova atualização. 
+                <p className="TextoParagrafo">Yoru, nativo do Japão, abre buracos na realidade para se infiltrar nas linhas inimigas invisíveis.
+                    Usando engano e agressão em medidas iguais, ele atinge cada alvo antes que eles saibam para onde olhar.
                 </p>
-                <a>Saiba Mais --</a>
+                <a className="SaibaMais">-- Saiba Mais --</a>
             </div>
         </div>
     );
